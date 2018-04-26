9d8ac9514e5c7b564ebb0d2b83e71060d9a37661
Ollie Day <OllieDay@users.noreply.github.com>
2018-04-26 20:13:35 +0100

    Add methods to set content

---
+++ Omicron.Tests/ContentExtensionsTests.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using MockableHttp;
+using NSubstitute;
+using Xunit;
+
+namespace Omicron.Tests
+{
+	public sealed class ContentExtensionTests
+	{
+		[Fact]
+		public async Task ShouldSetContentToHttpContentWithContent()
+			=> await SetContentAndVerifyIsSet(request => request.With.Content(new StringContent("...")), "...");
+
+		[Fact]
+		public async Task ShouldSetContentToByteArrayContentWithContent()
+		{
+			var content = new byte[] { 1, 2, 3 };
+
+			await SetContentAndVerifyIsSet(request => request.With.ByteArray(content), content);
+		}
+
+		[Fact]
+		public async Task ShouldSetContentToByteArrayContentWithContentAndOffsetAndCount()
+		{
+			var offset = 1;
+			var count = 1;
+
+			var content = new byte[] { 1, 2, 3 };
+			var expectedContent = content.Skip(offset).Take(count);
+
+			await SetContentAndVerifyIsSet(request => request.With.ByteArray(content, offset, count), expectedContent);
+		}
+
+		[Fact]
+		public async Task ShouldSetContentToFormUrlEncodedContentWithContent()
+		{
+			var content = new Dictionary<string, string>
+			{
+				["key"] = "value"
+			};
+
+			await SetContentAndVerifyIsSet(request => request.With.FormUrlEncoded(content), "key=value");
+		}
+
+		[Fact]
+		public async Task ShouldSetContentTypeHeaderToXWwwFormUrlEncodedWhenContentIsFormUrlEncodedContentWithContent()
+			=> await SetContentAndVerifyContentTypeHeaderIsSet(request => request.With.FormUrlEncoded(new Dictionary<string, string>()), "application/x-www-form-urlencoded");
+
+		[Fact]
+		public async Task ShouldSetContentToStringContentWithContent()
+			=> await SetContentAndVerifyIsSet(request => request.With.String("..."), "...");
+
+		[Fact]
+		public async Task ShouldSetContentTypeHeaderToTextPlainWhenContentIsStringContentWithContent()
+			=> await SetContentAndVerifyContentTypeHeaderIsSet(request => request.With.String("..."), "text/plain; charset=utf-8");
+
+		[Fact]
+		public async Task ShouldSetContentToStringContentWithContentAndEncoding()
+			=> await SetContentAndVerifyIsSet(request => request.With.String("...", Encoding.ASCII), "...");
+
+		[Fact]
+		public async Task ShouldSetContentTypeHeaderToTextPlainWhenContentIsStringContentWithContentAndEncoding()
+			=> await SetContentAndVerifyContentTypeHeaderIsSet(request => request.With.String("...", Encoding.ASCII), "text/plain; charset=us-ascii");
+
+		[Fact]
+		public async Task ShouldSetContentToStringContentWithContentAndEncodingAndMediaType()
+			=> await SetContentAndVerifyIsSet(request => request.With.String("...", Encoding.ASCII, "text/plain"), "...");
+
+		[Fact]
+		public async Task ShouldSetContentTypeHeaderToTextPlainWhenContentIsStringContentWithContentAndEncodingAndMediaType()
+			=> await SetContentAndVerifyContentTypeHeaderIsSet(request => request.With.String("...", Encoding.ASCII, "text/plain"), "text/plain; charset=us-ascii");
+
+		[Fact]
+		public async Task ShouldSetContentToStreamContentWithContent()
+		{
+			var content = new MemoryStream(new byte[] { 1, 2, 3 });
+
+			await SetContentAndVerifyIsSet(request => request.With.Stream(content), content);
+		}
+
+		[Fact]
+		public async Task ShouldSetContentToStreamContentWithContentAndBufferSize()
+		{
+			var content = new MemoryStream(new byte[] { 1, 2, 3 });
+
+			await SetContentAndVerifyIsSet(request => request.With.Stream(content, 8), content);
+		}
+
+		[Fact]
+		public async Task ShouldSetContentToJsonStringWithContent()
+			=> await SetContentAndVerifyIsSet(request => request.With.Json(new {}), "{}");
+
+		[Fact]
+		public async Task ShouldSetContentTypeHeaderToApplicationJsonWhenContentIsJsonString()
+			=> await SetContentAndVerifyContentTypeHeaderIsSet(request => request.With.Json(new {}), "application/json; charset=utf-8");
+
+		private static async Task SetContentAndVerifyIsSet(Action<IRequest> setter, string content)
+		{
+			var httpService = Substitute.For<IHttpService>();
+			var request = new Request(httpService, HttpMethod.Head, string.Empty);
+
+			setter(request);
+			request.Assert(Stubs.Noop);
+
+			await httpService.Received().SendAsync(Arg.Is<HttpRequestMessage>(httpRequestMessage =>
+				httpRequestMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult() == content
+			));
+		}
+
+		private static async Task SetContentAndVerifyIsSet(Action<IRequest> setter, IEnumerable<byte> content)
+		{
+			var httpService = Substitute.For<IHttpService>();
+			var request = new Request(httpService, HttpMethod.Head, string.Empty);
+
+			setter(request);
+			request.Assert(Stubs.Noop);
+
+			await httpService.Received().SendAsync(Arg.Is<HttpRequestMessage>(httpRequestMessage =>
+				httpRequestMessage.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult().SequenceEqual(content)
+			));
+		}
+
+		private static async Task SetContentAndVerifyIsSet(Action<IRequest> setter, Stream content)
+		{
+			var httpService = Substitute.For<IHttpService>();
+			var request = new Request(httpService, HttpMethod.Head, string.Empty);
+
+			setter(request);
+			request.Assert(Stubs.Noop);
+
+			await httpService.Received().SendAsync(Arg.Is<HttpRequestMessage>(httpRequestMessage =>
+				IsStreamContentSet(httpRequestMessage.Content.ReadAsStreamAsync().GetAwaiter().GetResult(), content)
+			));
+		}
+
+		private static bool IsStreamContentSet(Stream actual, Stream expected)
+		{
+			const int bufferSize = 16;
+
+			var actualBuffer = new byte[bufferSize];
+			var expectedBuffer = new byte[bufferSize];
+
+			actual.Read(actualBuffer, 0, bufferSize);
+
+			// Must seek to beginning before reading
+			expected.Seek(0, SeekOrigin.Begin);
+			expected.Read(expectedBuffer, 0, bufferSize);
+
+			return actualBuffer.SequenceEqual(expectedBuffer);
+		}
+
+		private static async Task SetContentAndVerifyContentTypeHeaderIsSet(Action<IRequest> setter, string value)
+		{
+			var httpService = Substitute.For<IHttpService>();
+			var request = new Request(httpService, HttpMethod.Head, string.Empty);
+
+			setter(request);
+			request.Assert(Stubs.Noop);
+
+			await httpService.Received().SendAsync(Arg.Is<HttpRequestMessage>(httpRequestMessage =>
+				IsContentTypeHeaderSet(httpRequestMessage.Content.Headers, value)
+			));
+		}
+
+		private static bool IsContentTypeHeaderSet(HttpContentHeaders headers, string value)
+		{
+			if (!headers.TryGetValues("Content-Type", out var values))
+			{
+				return false;
+			}
+
+			return values.Contains(value);
+		}
+	}
+}
---
+++ Omicron/ContentExtensions.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Omicron
+{
+	public static class ContentExtensions
+	{
+		public static IRequest Content(this IRequest @this, HttpContent content)
+			=> @this.Modify(request => request.Content = content);
+
+		public static IRequest ByteArray(this IRequest @this, byte[] content)
+			=> @this.Content(new ByteArrayContent(content));
+
+		public static IRequest ByteArray(this IRequest @this, byte[] content, int offset, int count)
+			=> @this.Content(new ByteArrayContent(content, offset, count));
+
+		public static IRequest FormUrlEncoded(this IRequest @this, IEnumerable<KeyValuePair<string, string>> nameValueCollection)
+			=> @this.Content(new FormUrlEncodedContent(nameValueCollection));
+
+		public static IRequest String(this IRequest @this, string content)
+			=> @this.Content(new StringContent(content));
+
+		public static IRequest String(this IRequest @this, string content, Encoding encoding)
+			=> @this.Content(new StringContent(content, encoding));
+
+		public static IRequest String(this IRequest @this, string content, Encoding encoding, string mediaType)
+			=> @this.Content(new StringContent(content, encoding, mediaType));
+
+		public static IRequest Multipart(this IRequest @this, params HttpContent[] values)
+			=> @this.Multipart(new MultipartContent(), values);
+
+		public static IRequest Multipart(this IRequest @this, string subtype, params HttpContent[] values)
+			=> @this.Multipart(new MultipartContent(subtype), values);
+
+		public static IRequest Multipart(this IRequest @this, string subtype, string boundary, params HttpContent[] values)
+			=> @this.Multipart(new MultipartContent(subtype, boundary), values);
+
+		public static IRequest MultipartFormData(this IRequest @this, params HttpContent[] values)
+			=> @this.MultipartFormData(new MultipartFormDataContent(), values);
+
+		public static IRequest MultipartFormData(this IRequest @this, string boundary, params HttpContent[] values)
+			=> @this.MultipartFormData(new MultipartFormDataContent(boundary), values);
+
+		public static IRequest Stream(this IRequest @this, Stream content)
+			=> @this.Content(new StreamContent(content));
+
+		public static IRequest Stream(this IRequest @this, Stream content, int bufferSize)
+			=> @this.Content(new StreamContent(content, bufferSize));
+
+		public static IRequest Json(this IRequest @this, object content)
+			=> @this.Content(new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json"));
+
+		private static IRequest Multipart(this IRequest @this, MultipartContent content, params HttpContent[] values)
+		{
+			return @this.Modify(request =>
+			{
+				foreach (var value in values)
+				{
+					content.Add(value);
+				}
+
+				request.Content = content;
+			});
+		}
+
+		private static IRequest MultipartFormData(this IRequest @this, MultipartFormDataContent content, params HttpContent[] values)
+		{
+			return @this.Modify(request =>
+			{
+				foreach (var value in values)
+				{
+					content.Add(value);
+				}
+
+				request.Content = content;
+			});
+		}
+	}
+}
